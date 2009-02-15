96d8df43e3b45a63d2d7f60ae8d43aab2c38362d
Karel Minarik <karmi@karmi.cz>
2009-02-16 00:20:37 +0100

    Initial commit -- first version

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Markout
+
+TextMate bundle to build *styled* HTML and PDF output of your Markdown text file.
---
+++ Support/bin/markout.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+$KCODE = 'UTF-8'
+
+require 'erb'
+
+tm_filename    = ENV['TM_FILENAME']  || 'untitled'
+tm_author      = ENV['TM_AUTHOR']
+
+basename = (tm_filename.split('.') - [tm_filename.split('.').last]).join('.')
+
+output_directory  = ENV['TM_DIRECTORY'] || '/tmp/'
+output_basename = File.join( output_directory, basename )
+
+template     = File.read File.join( File.dirname(__FILE__), '..', 'template.html.erb')
+screen_style = File.read File.join( File.dirname(__FILE__), '..', 'screen.css')
+print_style  = File.read File.join( File.dirname(__FILE__), '..', 'print.css')
+
+html_content = $stdin.read
+
+h1      = html_content.match(/<h1\s*.*>(.+)<\/h1>/)[1] rescue nil
+author  = ENV['TM_ORGANIZATION_NAME']
+title   = h1 || ENV['NAME'] || ''
+
+html = ERB.new(template).result
+
+html.gsub!(/[^\x00-\x7F]/) { |ch| "&##{ch.unpack("U")[0]};" }
+
+File.open("#{output_basename}.html", 'w') { |f| f.write html_content }
+
+print html
+
+`/opt/local/bin/htmldoc -f "#{output_basename}.pdf" --bodyfont "Helvetica" --headfootfont "Helvetica" --no-compression --color --embedfonts --header "" --footer .1. --links --no-title --toctitle "" --tocheader "..." --tocfooter "..." "#{output_basename}.html"`
+
+`open #{output_basename}.pdf`
