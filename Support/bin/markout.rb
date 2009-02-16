885451af83ff95e1ae881ac13277d4983526c454
Karel Minarik <karmi@karmi.cz>
2009-02-16 09:36:48 +0100

    CLEANUPS: Code cleanup, in-code documentation, confirmation dialog for generating PDF, open Preview.app in background.

--- Support/bin/markout.rb
+++ Support/bin/markout.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/env ruby
+
 $KCODE = 'UTF-8'
 
 require 'erb'
+require ENV['TM_SUPPORT_PATH'] + '/lib/ui.rb'
 
 tm_filename    = ENV['TM_FILENAME']  || 'untitled'
-tm_author      = ENV['TM_AUTHOR']
 
 basename = (tm_filename.split('.') - [tm_filename.split('.').last]).join('.')
-
 output_directory  = ENV['TM_DIRECTORY'] || '/tmp/'
 output_basename = File.join( output_directory, basename )
 
@@ -18,17 +18,23 @@ print_style  = File.read File.join( File.dirname(__FILE__), '..', 'print.css')
 html_content = $stdin.read
 
 h1      = html_content.match(/<h1\s*.*>(.+)<\/h1>/)[1] rescue nil
-author  = ENV['TM_ORGANIZATION_NAME']
+author  = ENV['TM_USER'] || ENV['TM_ORGANIZATION_NAME']
 title   = h1 || ENV['NAME'] || ''
 
 html_content = ERB.new(template).result
 
+# Adapted from MultiMarkdown bundle's "Convert document do PDF"
 html_content.gsub!(/[^\x00-\x7F]/) { |ch| "&##{ch.unpack("U")[0]};" }
 
+
+# == Write and display HTML
 File.open("#{output_basename}.html", 'w') { |f| f.write html_content }
-
 print html_content
+STDOUT.flush
 
+exit 0 unless TextMate::UI.request_confirmation( :title => 'Markout', :prompt => 'Generate PDF file as well?' )
+
+# == Write and display PDF
 `/opt/local/bin/htmldoc -f "#{output_basename}.pdf" --bodyfont "Helvetica" --headfootfont "Helvetica" --no-compression --color --embedfonts --header "" --footer .1. --links --no-title --toctitle "" --tocheader "..." --tocfooter "..." "#{output_basename}.html"`
 
-`open -a "Preview.app" #{output_basename}.pdf`
+`open -a "Preview.app" --background #{output_basename}.pdf`
